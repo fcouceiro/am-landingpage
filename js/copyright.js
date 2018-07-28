62333a4f4a1f591ff053366b9d8ec6b39509e8da
Francisco Couceiro <couceiro.f@gmail.com>
2018-07-28 12:28:56 +0100

    Fix copyright year + add fb link

--- js/copyright.js
+++ js/copyright.js
@@ -1,12 +1,11 @@
 (function ($) {
   "use strict"; // Start of use strict
+  $( document ).ready(function() {
+    $('#copyright-year').html(getCurrentYear())
+  });
 
   function getCurrentYear() {
     var d = new Date();
     return d.getFullYear();
   }
-  // Hide navbar when modals trigger
-  $('#copyright-year').innerHtml
-  console.log($('#copyright-year').innerHtml)
-
 })(jQuery); // End of use strict
