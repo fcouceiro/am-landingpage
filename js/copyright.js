ce69204e29bd41a486364984e66525c5c0a5a59a
Francisco Couceiro <couceiro.f@gmail.com>
2018-07-28 12:17:33 +0100

    Create AM Site + updated font awesome

---
+++ js/copyright.js
@@ -0,0 +1,12 @@
+(function ($) {
+  "use strict"; // Start of use strict
+
+  function getCurrentYear() {
+    var d = new Date();
+    return d.getFullYear();
+  }
+  // Hide navbar when modals trigger
+  $('#copyright-year').innerHtml
+  console.log($('#copyright-year').innerHtml)
+
+})(jQuery); // End of use strict
